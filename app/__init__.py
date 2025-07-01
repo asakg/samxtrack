6a764b4a19f4651cd86e8d69f37a337003b74671
Aslan Alybaev <aaslan@Aslans-MacBook-Air.local>
2025-07-01 12:42:09 -0500

    initial login page setup2

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_session import Session
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = "replace_this_with_a_secure_key"
+    app.secret_key = "super-secret-key"  # Change this in production!
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
 
-    from .routes.dashboard import dashboard_bp
+    # Register routes
+    from app.routes.login import login_bp
+    from app.routes.dashboard import dashboard_bp
+    app.register_blueprint(login_bp)
     app.register_blueprint(dashboard_bp)
 
     return app
